5f284a16e76af6f807e5a89e079593624e3f1c49
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-04-02 17:04:21 +0200

    sea-level.bin

--- extract-sea-levels.pl
+++ extract-sea-levels.pl
@@ -1,12 +1,19 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
+# use Config;
+
+# die unless $Config{shortsize} == 2;
+# exit;
 
 open (my $in , '<', "$ENV{digitales_backup}development/Daten/GeoSpatial-etc/ETOPO1/0.25.xyz") or die $!;
-open (my $out, '>', "sea-level.js") or die;
+#open (my $out, '>', "sea-level.js") or die;
+open (my $out, '>', "sea-level.bin") or die;
+binmode $out;
 
-print $out "var z=[";
+# print $out "var z=[";
 
+my $cnt = 0;
 for (my $y=  90; $y >= -90; $y -= 0.25) {
 for (my $x=-180; $x <  180; $x += 0.25) {
 
@@ -14,7 +21,10 @@ for (my $x=-180; $x <  180; $x += 0.25) {
     die unless defined $line;
     die "x: $x, y: $y\nline: $line" unless $line =~ /^$x\t$y\t(-?\d+)$/;
     my $z = $1;
-    print $out "$z,";
+#   print $out "$z,";
+#   print $out pack('v', $z); # v:  An unsigned short (16-bit), little Endian
+    print $out pack('s', $z); # s:  An   signed short (16-bit)
+    $cnt ++;
 
   }
   my $x=180;
@@ -22,4 +32,6 @@ for (my $x=-180; $x <  180; $x += 0.25) {
   die "x: $x, y: $y\nline: $line" unless $line =~ /^$x\t$y\t(-?\d+)$/;
 }
 
-print $out "];\n";
+# print $out "];\n";
+
+print "$cnt bytes written\n";
